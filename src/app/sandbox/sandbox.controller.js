7a04fb0410b3564c2dbea136d2d07e9d72712b87
Riccardo Causo <riccardo.causo@lugano.ch>
2015-12-04 16:18:55 +0100

    sandy my girl

--- src/app/sandbox/sandbox.controller.js
+++ src/app/sandbox/sandbox.controller.js
@@ -5,15 +5,29 @@
         .module('app.sandbox')
         .controller('SandboxController', SandboxController);
 
-    /* SandboxController.$inject = ['$q','logger','sandboxdataservice','dummylist']; */
+    SandboxController.$inject = ['$timeout'];
     /* @ngInject */
-    function SandboxController() {
+    function SandboxController($timeout) {
         var vm = this;
         vm.news = {
             title: 'ciao',
             description: 'Hot Towel Angular is a SPA template for Angular developers.'
         };
 
+        vm.name = 'Tobias';
+        vm.message = '';
+        vm.dialogIsHidden = false;
+        vm.hideDialog = hideDialog;
+
+        function hideDialog(message){
+            console.log('nascondone...');
+            vm.message = message;
+            vm.dialogIsHidden = true;
+            $timeout(function () {
+                vm.message = '';
+                vm.dialogIsHidden = false;
+            }, 2000);
+        }
 
     }
 })();
--- src/app/sandbox/sandbox.directive.js
+++ src/app/sandbox/sandbox.directive.js
@@ -8,7 +8,7 @@
     function sandy() {
         var directive = {
             restrict: 'EA',
-            templateUrl: 'src/sandbox/sandy.directive.html',
+            templateUrl: 'app/sandbox/sandy.directive.html',
             scope: {
                 max: '='
             },
@@ -43,4 +43,44 @@
         console.log('CTRL: vm.max = %s', vm.max);
     }
 
+    angular
+        .module('app.sandbox')
+        .directive('myDialog', myDialog);
+
+    function myDialog() {
+        return {
+            restrict: 'E',
+            transclude: true,
+            link: link,
+            controller: MyDialogController,
+            // note: This would be 'ExampleController' (the exported controller name, as string)
+            // if referring to a defined controller in its separate file.
+            controllerAs: 'vm',
+            bindToController: true, // because the scope is isolated
+            scope: {
+                'close': '&onClose'
+            },
+            templateUrl: 'app/sandbox/my-dialog-close.html'
+        };
+        function link(scope,elem,attr){
+            console.log("FUNK!");
+            console.log(scope.close);
+            scope.prova = 'prozza ciao';
+
+
+        }
+
+        function MyDialogController(){
+            var vm = this;
+            vm.chiudi = function(){
+                console.log("chiudi li");
+                //vm.ciccio({message: 'crepa'});
+                //vm.close({message: 'crepa'});
+                var message = 'poldone';
+                vm.close()(message);
+            };
+
+        }
+
+    }
 })();
